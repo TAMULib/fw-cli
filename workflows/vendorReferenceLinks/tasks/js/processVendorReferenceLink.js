4179fc91a9977b856749311e4224d49f85cdb800
William Welling <wwelling@library.tamu.edu>
2019-11-05 21:49:22 -0600

    fixed vendor reference link processor

--- workflows/vendorReferenceLinks/tasks/js/processVendorReferenceLink.js
+++ workflows/vendorReferenceLinks/tasks/js/processVendorReferenceLink.js
@@ -1,10 +1,10 @@
 var vendorTypes = {
-  AMDB: '{{mod-external-reference-resolver}}/referenceLinkTypes/08c7dd18-dbaf-11e9-8a34-2a2ae2dbcce4',
-  MSDB: '{{mod-external-reference-resolver}}/referenceLinkTypes/08c7df8e-dbaf-11e9-8a34-2a2ae2dbcce4'
+  VENDOR_AMDB: '{{mod-external-reference-resolver}}/referenceLinkTypes/08c7dd18-dbaf-11e9-8a34-2a2ae2dbcce4',
+  VENDOR_MSDB: '{{mod-external-reference-resolver}}/referenceLinkTypes/08c7df8e-dbaf-11e9-8a34-2a2ae2dbcce4'
 };
 
 var returnObj = {
   folioReference: UUID.randomUUID().toString(),
   externalReference: args.VENDOR_ID,
-  type: args.SCHEMA.toLowerCase() === 'amdb' ? vendorTypes.AMDB : vendorTypes.MSDB
+  type: vendorTypes[args.SCHEMA]
 };
