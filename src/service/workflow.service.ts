57479476f2c34a0d3535e84a34d83302965ded71
William Welling <wwelling@library.tamu.edu>
2019-11-08 15:42:59 -0600

    replace tabs and multiple spaces with single space, replcae double quotes with single quotes

--- src/service/workflow.service.ts
+++ src/service/workflow.service.ts
@@ -145,9 +145,9 @@ class WorkflowService extends RestService implements Enhancer {
           // remove all endline characters
           .replace(/(\r\n|\n|\r)/gm, '')
           // remove all extraneous double spaces
-          .replace(/\s+/g, ' ')
+          .replace(/\s\s+/g, ' ')
           // replace all double quotes with single quotes
-          .replace(/'/g, '\'');
+          .replace(/"/g, '\'');
       }
 
     }
