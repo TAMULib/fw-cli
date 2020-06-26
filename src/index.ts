b0436eaee9469187e2a01d01fa8c76ef1d528981
William Welling <wwelling@library.tamu.edu>
2020-06-26 09:49:25 -0500

    add rules update to cli

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ import { fileService } from './service/file.service';
 import { defaultService } from './service/default.service';
 import { referenceData } from './service/reference-data.service';
 import { referenceLinks } from './service/reference-links.service';
+import { pathToFileURL } from 'url';
+import { mappingRules } from './service/mapping-rules.service';
 
 const CONF_DIR = 'configs';
 
@@ -175,7 +177,7 @@ program
   });
 
 program
-  .command('load <workflow> <type> ')
+  .command('load <workflow> <type>')
   .description('load reference data/links for an existing workflow')
   .action((workflow: string, type: 'data' | 'links') => {
     console.log(`load reference ${type} for ${workflow}`);
@@ -183,12 +185,14 @@ program
     if (fileService.exists(workflowPath)) {
       switch (type) {
         case 'data':
-          referenceData.createReferenceData(workflow);
-          console.log(`reference ${type} loading for ${workflow}`);
+          referenceData.createReferenceData(workflow).then(() => {
+            console.log(`reference ${type} loaded for ${workflow}`);
+          });
           break;
         case 'links':
-          referenceLinks.createReferenceLinkTypes(workflow);
-          console.log(`reference ${type} loaded for ${workflow}`);
+          referenceLinks.createReferenceLinkTypes(workflow).then(() => {
+            console.log(`reference ${type} loaded for ${workflow}`);
+          });
           break;
         default:
           console.log(`reference ${type} not supported. <data/links>`);
@@ -196,6 +200,28 @@ program
       }
     } else {
       console.log(`${workflow} workflow does not exist`);
+    }
+  });
+
+program
+  .command('rules <type> [path]')
+  .description('update mapping rules for instances with optional path')
+  .action((type: 'instances', path: string) => {
+    const file = path === undefined ? `${config.get('wd')}/instance_mapping_rules.json` : path;
+    console.log(`update ${type} mapping rules ${file}`);
+    if (fileService.exists(file)) {
+      switch (type) {
+        case 'instances':
+          mappingRules.update(file).then(() => {
+            console.log(`${type} rules updated from ${file}`);
+          });
+          break;
+        default:
+          console.log(`${type} rules not supported. <instances>`);
+          return;
+      }
+    } else {
+      console.log(`${file} does not exist`);
     }
   });
 
---
+++ src/service/mapping-rules.service.ts
@@ -0,0 +1,26 @@
+import { RestService } from './rest.service';
+
+import { config } from '../config';
+import { fileService } from './file.service';
+import { okapi } from './okapi.service';
+
+class MappingRulesService extends RestService {
+
+  public update(path: string, endpoint: string = 'mapping-rules'): Promise<any> {
+    if (fileService.exists(path)) {
+      const rules = fileService.read(path);
+      console.log(rules);
+      if (rules.length === 0) {
+        return Promise.resolve();
+      }
+      return [
+        () => okapi.login(),
+        () => okapi.put(`${config.get('okapi')}/${endpoint}`, JSON.parse(rules))
+      ].reduce((prevPromise, process) => prevPromise.then(() => process(), () => process()), Promise.resolve());
+    }
+    return Promise.reject(`cannot find rules at ${path}`);
+  }
+
+}
+
+export const mappingRules = new MappingRulesService();
