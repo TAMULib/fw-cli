954000ff7045b886e7d4f2ee87db535c5223aed4
William Welling <wwelling@tamu.edu>
2024-11-20 18:27:02 -0600

    Use forEach to iterate over cookie values

--- src/service/cookie.utility.ts
+++ src/service/cookie.utility.ts
@@ -5,10 +5,10 @@ export const parseCookie = (cookie: string = ''): Map<string, string> => {
     const cookieMap = new Map<string, string>();
     const cookies = cookie.split(SEMICOLON);
 
-    for (let i = 0; i <= cookies.length; ++i) {
-        const keyValue = cookies[i].split(EQUAL);
-        cookieMap.set(keyValue[0], keyValue[1])
-    }
+    cookies.forEach((c: string) => {
+        const keyValue = c.split(EQUAL);
+        cookieMap.set(keyValue[0], keyValue[1]);
+    });
 
     return cookieMap;
 };
